8420002b7bc49b7f7b487c2332a15c135ad04ba2
crynchypi <crunchypi@protonmail.com>
2020-07-31 20:46:56 +0200

    initial ---rough--- work on neo4j implementation

---
+++ src/storage/neo4j/neo4j.go
@@ -0,0 +1,163 @@
+package neo4j
+
+import (
+	"github.com/crunchypi/markov-go-sql.git/src/protocols"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+// # Neo4jManager implements protocols.DBAbstracter
+var _ protocols.DBAbstracter = (*Neo4jManager)(nil)
+
+type Neo4jManager struct {
+	db neo4j.Driver
+}
+
+// Used internal as args in main execution function.
+type executeParams struct {
+	cypher       string
+	bindings     map[string]interface{}
+	callbackMode bool
+	callback     func(neo4j.Result) // Optional
+}
+
+func New(uri, user, pwd string, encr bool) (protocols.DBAbstracter, error) {
+	new := Neo4jManager{}
+
+	driver, err := neo4j.NewDriver(
+		uri,
+		neo4j.BasicAuth(user, pwd, ""),
+		func(c *neo4j.Config) {
+			c.Encrypted = encr
+		})
+	if err != nil {
+		return &new, err
+	}
+	new.db = driver
+	return &new, nil
+}
+
+// modifier is the point of contact of the neo4j db/driver.
+// @ TODO: transactions + batched commands?
+func (n *Neo4jManager) execute(x executeParams) error {
+	// # Open.
+	session, err := n.db.Session(neo4j.AccessModeWrite)
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+
+	// # Execute.
+	res, err := session.Run(x.cypher, x.bindings)
+	if err != nil {
+		return err
+	}
+
+	// # Optional callback.
+	if x.callbackMode {
+		for res.Next() {
+			x.callback(res)
+		}
+	}
+	return nil
+}
+
+// bindings does a common task in this file: converts
+// arguments into a map suitable for the sql pkg
+func (n *Neo4jManager) bindings(word, other string, dst int) map[string]interface{} {
+	return map[string]interface{}{
+		"word":  word,
+		"other": other,
+		"dst":   dst,
+	}
+}
+
+func (n *Neo4jManager) newNode(word string) error {
+	return n.execute(executeParams{
+		cypher: `CREATE (:MChain {word:$word})`,
+		bindings: map[string]interface{}{
+			"word": word,
+		},
+	})
+}
+
+func (n *Neo4jManager) getNode(word string) []interface{} {
+	res := make([]interface{}, 0, 10) // 10 is arbitrary.
+	// res := []string{}
+	n.execute(executeParams{
+		cypher: `MATCH (x) WHERE x.word = $word RETURN x.word AS w`,
+		bindings: map[string]interface{}{
+			"word": word,
+		},
+		callbackMode: true,
+		callback: func(r neo4j.Result) {
+			item, ok := r.Record().Get("w")
+			if ok {
+				res = append(res, item)
+			}
+		},
+	})
+	return res
+}
+
+func (n *Neo4jManager) nodeExists(word string) bool {
+	return len(n.getNode(word)) > 0
+}
+
+func (n *Neo4jManager) newRelationship(word, other string, dst int) error {
+	return n.execute(executeParams{
+		cypher: `
+			MATCH (x), (y)
+			WHERE x.word = $word
+			  AND y.word = $other
+		   CREATE (x)-[:conn{dst:$dst, count:1}]->(y)
+		`,
+		bindings: map[string]interface{}{
+			"word":  word,
+			"other": other,
+			"dst":   dst,
+		},
+	})
+}
+
+func (n *Neo4jManager) getRelationShip(word, other string, dst int) (error, []interface{}) {
+	res := make([]interface{}, 0, 10) // # 10 is arbitrary
+	err := n.execute(executeParams{
+		cypher: `
+			 MATCH (x:MChain{word:$word})-[r:conn{dst:1}]->(y:MChain{word:$other})
+			RETURN x.word AS a, y.word AS b, r.dst AS c, r.count AS d
+		`,
+		bindings: map[string]interface{}{
+			"word":  word,
+			"other": other,
+		},
+		callbackMode: true,
+		callback: func(r neo4j.Result) {
+			a, aok := r.Record().Get("a")
+			b, bok := r.Record().Get("b")
+			c, cok := r.Record().Get("c")
+			d, dok := r.Record().Get("d")
+			if aok && bok && cok && dok {
+				res = append(res, a, b, c, d)
+			}
+			// res = append(res, r.Record().Keys())
+		},
+	})
+	return err, res
+}
+
+func (n *Neo4jManager) relationshipExists(word, other string, dst int) bool {
+	res := false
+	return res
+}
+
+func (n *Neo4jManager) IncrementPair(word, other string, dst int) {
+	// Ignore err.
+	var _ = n.execute(executeParams{
+		cypher: ``,
+	})
+}
+
+func (n *Neo4jManager) SucceedingX(word string) []protocols.WordRelationship {
+
+	return make([]protocols.WordRelationship, 0)
+}
---
+++ src/storage/neo4j/neo4j_test.go
@@ -0,0 +1,153 @@
+package neo4j
+
+import (
+	"log"
+	"testing"
+
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
+
+const (
+	uri = "bolt://localhost:7687"
+	usr = ""
+	pwd = ""
+	enc = false
+)
+
+func init() {
+	if usr == "" || pwd == "" {
+		panic("credentials not set")
+	}
+}
+
+func tryCleanup() {
+
+	n, err1 := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+	err2 := m.execute(executeParams{
+		cypher:   `MATCH (x) DETACH DELETE x;`,
+		bindings: make(map[string]interface{}, 0),
+	})
+	if err1 != nil || err2 != nil {
+		log.Println("Failed while cleaning up")
+	}
+}
+
+func TestNew(t *testing.T) {
+	_, err := New(uri, usr, pwd, enc)
+	if err != nil {
+		t.Error("failed to create new Neo4jManager instance:", err)
+	}
+}
+
+// # Written to be inspected manually.
+func TestModifierNew(t *testing.T) {
+	tryCleanup()
+	n, _ := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+	m.execute(executeParams{
+		cypher:   `CREATE (_:TestNode {t:$val})`,
+		bindings: map[string]interface{}{"val": 99},
+	})
+}
+
+func TestModifierCallback(t *testing.T) {
+	tryCleanup()
+	n, _ := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+
+	val := 44
+	// # Create data.
+	m.execute(executeParams{
+		cypher:   `CREATE (_:TestNode {t:$val})`,
+		bindings: map[string]interface{}{"val": val},
+	})
+	// # Read data
+	res := make([]interface{}, 0, 0)
+	callback := func(r neo4j.Result) {
+		res = r.Record().Values()
+	}
+	m.execute(executeParams{
+		cypher:       `MATCH (x) WHERE x.t = $val RETURN x.t`,
+		bindings:     map[string]interface{}{"val": val},
+		callbackMode: true,
+		callback:     callback,
+	})
+	// # Check (unsafely)
+	if res[0].(int64) != int64(val) {
+		t.Error("Unexpected fetch result.")
+	}
+}
+
+// # created to be checked manually.
+func TestNewNode(t *testing.T) {
+	tryCleanup()
+	n, _ := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+	err := m.newNode("testword")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// # created to be checked manually.
+func TestNodeExists(t *testing.T) {
+	tryCleanup()
+	n, _ := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+
+	word := "test"
+	if m.nodeExists(word) {
+		t.Error("unexpected: node exists")
+	}
+	m.newNode(word)
+	if !m.nodeExists(word) {
+		t.Error("unexpected: node does not exist")
+	}
+}
+
+func TestGetNode(t *testing.T) {
+	tryCleanup()
+	n, _ := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+
+	word := "test"
+	m.newNode(word)
+	r := m.getNode(word)
+	if r[0].(string) != word {
+		t.Error("not found")
+	}
+}
+
+// # Written to be tested manually.
+func TestNewRelationship(t *testing.T) {
+	tryCleanup()
+	n, _ := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+
+	x, y := "x", "y"
+	m.newNode(x)
+	m.newNode(y)
+	m.newRelationship(x, y, 1)
+}
+
+func TestGetRelationship(t *testing.T) {
+	tryCleanup()
+	n, _ := New(uri, usr, pwd, enc)
+	m := n.(*Neo4jManager)
+
+	nodeA, nodeB := "a", "b"
+	dst := 1
+	m.newNode(nodeA)
+	m.newNode(nodeB)
+	m.newRelationship(nodeA, nodeB, dst)
+
+	_, res := m.getRelationShip(nodeA, nodeB, dst)
+	// t.Log(err, res)
+	if res[0].(string) != nodeA &&
+		res[1].(string) != nodeB &&
+		res[2].(int) != dst &&
+		res[3].(int) != 1 {
+		t.Error("didnt get rel:", res)
+	}
+}
